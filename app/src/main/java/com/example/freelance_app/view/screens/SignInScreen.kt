6444c429db0592626b26056d17dc3043a8acb29b
Son Minh Nguyen <72074194+SonMinhNguyen2000@users.noreply.github.com>
2021-11-06 14:06:51 -0700

    add UI not link yet

--- app/src/main/java/com/example/freelance_app/view/screens/SignInScreen.kt
+++ app/src/main/java/com/example/freelance_app/view/screens/SignInScreen.kt
@@ -1,9 +1,67 @@
 package com.example.freelance_app.view.screens
 
-import androidx.compose.runtime.Composable
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.freelance_app.R
+import com.example.freelance_app.ui.theme.FreelanceAppTheme
+import com.example.freelance_app.ui.theme.fontSizeLarge
+import com.example.freelance_app.ui.theme.fontSizeMed
+import com.example.freelance_app.view.reusables.CustomButton
+import com.example.freelance_app.view.reusables.InputField
+
+
 
 @Composable
 fun SignInScreen(navController: NavController) {
+    var email by remember { mutableStateOf("") }
+    Column(modifier = Modifier.padding(20.dp),
+           horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(R.drawable.google_text),
+              contentDescription = null,
+              modifier = Modifier.size(100.dp))
+        Text(text="Sign in",
+             fontSize = fontSizeLarge,
+             fontWeight = FontWeight.SemiBold)
+        Spacer(modifier = Modifier.size(5.dp))
+        Text(text="with your Google Account. Learn more",
+             fontSize = fontSizeMed)
+        Spacer(modifier = Modifier.size(15.dp))
+        InputField("Email or phone",
+            "",
+            onValueChange = { email = it },
+            keyboardType = KeyboardType.Email,
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column() {
+                Text(text="Forgot password?",
+                    fontSize = fontSizeMed)
+                Spacer(modifier = Modifier.size(30.dp))
+                Text(text="Create account",
+                    fontSize = fontSizeMed)
+            }
+        }
+        Row(modifier= Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.End){
+            CustomButton(
+                modifier = Modifier
+                    .height(50.dp).width(80.dp),
+                onClick = {  },
+            ) {
+                Text(text = "Next")
+            }
+        }
 
+    }
 }
--- app/src/main/java/com/example/freelance_app/view/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/freelance_app/view/screens/WelcomeScreen.kt
@@ -1,21 +1,20 @@
 package com.example.freelance_app.view.screens
 
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.freelance_app.ui.theme.White
+import com.example.freelance_app.R
+import com.example.freelance_app.ui.theme.*
 import com.example.freelance_app.view.reusables.*
 
 
@@ -23,11 +22,69 @@ import com.example.freelance_app.view.reusables.*
 // These are testing for reusable components if you are going to work on this page
 // You can delete them or keep them to check if you are not sure how to use these
 
-
-@ExperimentalComposeUiApi
-@ExperimentalAnimationApi
 @Composable
 fun WelcomeScreen(navController: NavController) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colors.primaryVariant)
+        .padding(top = 5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Row{
+            Image(painter = painterResource(R.drawable.logo_onlytext),
+                contentDescription = null,
+                modifier= Modifier.size(250.dp))
+        }
+
+        Text(
+            text="A solution for your emergency business",
+            fontSize = fontSizeLarge,
+            fontWeight = FontWeight.ExtraBold,
+            color = White,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(200.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.weight(2f),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Text (text="Are you an",
+                    fontSize = fontSizeLarge,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = White
+                )
+            }
+            Spacer(modifier = Modifier.height(30.dp).weight(1f))
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        CustomButton(
+            modifier = Modifier
+                .height(50.dp),
+            onClick = {  },
+        ) {
+            Text(text = "Employer")
+        }
+        Text (text="Or",
+            fontSize = fontSizeMed,
+            color = White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(10.dp)
+        )
+        CustomButton(
+            modifier = Modifier
+                .height(50.dp),
+            onClick = {  },
+        ) {
+            Text(text = "Applicant")
+        }
+    }
+}
+
+
+
+
+
 //    var email by remember { mutableStateOf("") }
 //
 //    Column {
@@ -53,4 +110,4 @@ fun WelcomeScreen(navController: NavController) {
 //            Spacer(modifier = Modifier.size(5.dp))
 //            Avatar(imageUrl = "https://i1.kknews.cc/SIG=p567ci/3155000qo23rs0nr60p2.jpg")
 //        }
-}
+
