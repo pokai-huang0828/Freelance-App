a4b5654d2fd1e8c29510b9fd632f88fa3596a456
Po-Kai Huang <000429977@student.vcc.ca>
2021-11-05 21:54:54 -0700

    Added Color palette

--- app/src/main/java/com/example/freelance_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/freelance_app/ui/theme/Color.kt
@@ -1,8 +1,14 @@
 package com.example.freelance_app.ui.theme
 
+import android.graphics.Color.rgb
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val BlackBlue = Color(rgb(27, 38, 44))
+val DarkBlue = Color(rgb(15, 76, 117))
+val MidBlue = Color(rgb(50, 130, 184))
+val LightBlue = Color(rgb(187, 225, 250))
